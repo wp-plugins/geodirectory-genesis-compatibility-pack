c9eecbb806863ae45efcf4458da1e52fd0b93a38
paoltaia <paoltaia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-24 22:16:46 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/geodirectory-genesis-compatibility-pack/trunk@954457 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ geodir_genesis_compatibility.php
@@ -0,0 +1,146 @@
+<?php
+/*
+Plugin Name: GeoDirectory - Genesis Theme Compatibility
+Plugin URI: http://wpgeodirectory.com
+Description: This plugin lets the GeoDirectory Plugin use the Genesis theme HTML wrappers to fit and work perfectly.
+Version: 1.0.1
+Author: GeoDirectory
+Author URI: http://wpgeodirectory.com
+
+*/
+
+
+// BECAUSE THIS PLUGIN IS CALLED BEFORE GD WE MUST CALL THIS PLUGIN ONCE GD LOADS
+add_action( 'plugins_loaded', 'geodir_genesis_action_calls', 10 );
+function geodir_genesis_action_calls(){
+	
+	/* ACTIONS
+	****************************************************************************************/
+	// LOAD STYLESHEET
+	add_action( 'wp_enqueue_scripts', 'geodir_genesis_styles' );
+	
+	// Add body class for styling purposes
+	add_filter('body_class','geodir_genesis_body_class');
+	
+	// HOME TOP SIDEBAR
+	remove_action( 'geodir_home_before_main_content', 'geodir_action_geodir_sidebar_home_top', 10 );
+	add_action( 'genesis_after_header', 'geodir_genesis_home_sidebar', 21 );
+	add_action( 'geodir_before_search_form', 'geodir_genesis_search_container_open' );
+	add_action( 'geodir_after_search_form', 'geodir_genesis_search_container_close' );
+	
+	// WRAPPER OPEN ACTIONS
+	remove_action( 'geodir_wrapper_open', 'geodir_action_wrapper_open', 10 );
+	add_action( 'geodir_wrapper_open', 'geodir_genesis_action_wrapper_open', 9 );
+	
+	// WRAPPER CLOSE ACTIONS
+	remove_action( 'geodir_wrapper_close', 'geodir_action_wrapper_close', 10);
+	add_action( 'geodir_wrapper_close', 'geodir_genesis_action_wrapper_close', 11);	
+	
+	// WRAPPER CONTENT OPEN ACTIONS
+	remove_action( 'geodir_wrapper_content_open', 'geodir_action_wrapper_content_open', 10 );
+	add_action( 'geodir_wrapper_content_open', 'geodir_genesis_action_wrapper_content_open', 9, 3 );
+	
+	// WRAPPER CONTENT CLOSE ACTIONS
+	remove_action( 'geodir_wrapper_content_close', 'geodir_action_wrapper_content_close', 10);
+	add_action( 'geodir_wrapper_content_close', 'geodir_genesis_action_wrapper_content_close', 11);
+	
+	// SIDEBAR RIGHT OPEN ACTIONS
+	remove_action( 'geodir_sidebar_right_open', 'geodir_action_sidebar_right_open', 10 );
+	add_action( 'geodir_sidebar_right_open', 'geodir_genesis_action_sidebar_right_open', 10, 4 );
+	
+	// SIDEBAR RIGHT CLOSE ACTIONS
+	remove_action( 'geodir_sidebar_right_close', 'geodir_action_sidebar_right_close', 10);
+	add_action( 'geodir_sidebar_right_close', 'geodir_genesis_action_sidebar_right_close', 10,1);
+	
+	// ADD .entry CLASS TO <article> FOR LISTINGS
+	add_filter('geodir_post_view_article_extra_class' ,'geodir_genesis_post_view_article_class');
+	
+	// REPLACE GENESIS BREADCRUMBS WITH GD BREADCRUMBS
+	remove_action( 'geodir_detail_before_main_content', 'geodir_breadcrumb', 20 );
+	remove_action( 'geodir_listings_before_main_content', 'geodir_breadcrumb', 20 );
+	remove_action( 'geodir_author_before_main_content', 'geodir_breadcrumb', 20 );
+	remove_action( 'geodir_search_before_main_content', 'geodir_breadcrumb', 20 );
+	remove_action( 'geodir_home_before_main_content', 'geodir_breadcrumb', 20 );
+	add_action( 'genesis_after_header', 'geodir_replace_breadcrumb', 20 );
+	
+} // Close geodir_genesis_action_calls
+
+/* FUNCTIONS
+****************************************************************************************/
+
+function geodir_genesis_post_view_article_class($grid_view_class) {
+	$grid_view_class = 'entry';
+	return $grid_view_class;
+}
+
+// ENQUEUE STYLESHEET & ADD BODY CLASS
+function geodir_genesis_styles() {
+    // Register the style like this for a plugin:
+    wp_register_style( 'geodir-genesis-style', plugins_url( '/css/plugin.css', __FILE__ ) );
+    wp_enqueue_style( 'geodir-genesis-style' );
+}
+
+function geodir_genesis_body_class($classes) {
+	$classes[] = 'geodir-genesis';
+	return $classes;
+}
+
+// REPLACE HOME TOP SIDEBAR AFTER HEADER
+function geodir_genesis_home_sidebar() {
+		global $wp;
+		if ( $wp->query_vars['page_id'] == get_option( 'geodir_location_page' ) || is_home() && !$_GET['geodir_signup'] ) {
+        	dynamic_sidebar('geodir_home_top');
+		}
+}
+
+// ADD OPENING WRAP TO SEARCHBAR
+function geodir_genesis_search_container_open() {
+	echo '<div class="wrap">';
+}
+
+// ADD CLOSING WRAP TO SEARCHBAR
+function geodir_genesis_search_container_close() {
+	echo '</div>';
+}
+
+// WRAPPER OPEN FUNCTIONS
+function geodir_genesis_action_wrapper_open(){
+	echo '<div class="content-sidebar-wrap">';
+}
+
+// WRAPPER CLOSE FUNCTIONS
+function geodir_genesis_action_wrapper_close(){
+	echo '</div>';
+}
+
+// WRAPPER CONTENT OPEN FUNCTIONS
+function geodir_genesis_action_wrapper_content_open($type='',$id='',$class=''){
+		echo '<main class="content '. $class .'" itemprop="mainContentOfPage" role="main">';
+}
+
+// WRAPPER CONTENT CLOSE FUNCTIONS
+function geodir_genesis_action_wrapper_content_close(){
+	echo '</main>';
+}
+
+// SIDEBAR RIGHT OPEN FUNCTIONS
+function geodir_genesis_action_sidebar_right_open($type='',$id='',$class='',$itemtype=''){
+	echo '<aside class="sidebar sidebar-primary widget-area" itemtype="http://schema.org/WPSideBar" itemscope="itemscope" role="complementary">';
+}
+
+// SIDEBAR RIGHT CLOSE FUNCTIONS
+function geodir_genesis_action_sidebar_right_close($type=''){
+	echo '</aside>';
+}
+
+// REPLACE GENESIS BREADCRUMBS FUNCTION
+function geodir_replace_breadcrumb() {
+	echo '<div class="wrap">';
+	geodir_breadcrumb();
+	echo '</div>';
+}
+
+
+
+
+
